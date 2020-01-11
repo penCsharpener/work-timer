a2398fead29acfde2ee05b46560f29f9af8758f3
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-01-11 15:34:18 +0100

    WorkPeriod: make EndTime nullable

--- src/WorkTimer.Models/Models/WorkPeriod.cs
+++ src/WorkTimer.Models/Models/WorkPeriod.cs
@@ -9,7 +9,7 @@ namespace WorkTimer.Models {
         public int Id { get; set; }
         public int WorkingDayId { get; set; }
         public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime? EndTime { get; set; }
         public string? Comment { get; set; }
 
         [Computed]
