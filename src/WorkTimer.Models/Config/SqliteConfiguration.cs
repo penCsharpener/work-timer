5400f9d7911c14a124b117b0e079fc8b3e623632
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-01-15 21:26:51 +0100

    add SqliteConfiguration and extension to form connection string

---
+++ src/WorkTimer.Models/Config/SqliteConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkTimer.Config {
+    public class SqliteConfiguration {
+        public string PathToDatabase { get; set; } = "%appdata%\\WorkTimer";
+        public string DatabaseFileName { get; set; } = "worktimer.db";
+        public string DatabasePassword { get; set; } = "";
+
+    }
+}
---
+++ src/WorkTimer.Models/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WorkTimer.Config;
+
+namespace WorkTimer {
+    public static class Extensions {
+        public static string ToConnectionString(this SqliteConfiguration config) {
+            return $"Data Source={Path.Combine(Environment.ExpandEnvironmentVariables(config.PathToDatabase), config.DatabaseFileName)};Version=3;" +
+                $"{(string.IsNullOrEmpty(config.DatabasePassword) ? "" : $"Password={config.DatabasePassword};")}" +
+                $"Compress=True;UTF8Encoding=True;";
+        }
+    }
+}
