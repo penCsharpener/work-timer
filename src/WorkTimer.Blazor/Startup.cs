9a65ebf07d9f5ac6e2f497d319c21d77fad711ce
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-02-08 00:04:34 +0100

    add EFCore identity to blazor app

--- src/WorkTimer.Blazor/Startup.cs
+++ src/WorkTimer.Blazor/Startup.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Data.SQLite;
 using WorkTimer.Config;
 using WorkTimer.Contracts;
+using WorkTimer.EF;
+using WorkTimer.Models;
 using WorkTimer.Repositories;
 using WorkTimer.Services;
 
@@ -21,6 +26,13 @@ namespace WorkTimer.Blazor {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
+            services.AddDbContextPool<AppDbContext>(options => {
+                options.UseSqlite(Environment.ExpandEnvironmentVariables(Configuration.GetConnectionString("SqliteEFCoreIdentity")), opt => {
+                    opt.MigrationsAssembly("WorkTimer.EF");
+                });
+            });
+            //services.AddEntityFrameworkSqlite();
+            services.AddIdentity<AppUser, IdentityRole<int>>().AddEntityFrameworkStores<AppDbContext>();
             services.AddServerSideBlazor();
             services.Configure<SqliteConfiguration>(Configuration.GetSection(nameof(SqliteConfiguration)));
             services.AddScoped<IWorkPeriodWriter, WorkPeriodWriter>();
@@ -50,6 +62,8 @@ namespace WorkTimer.Blazor {
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
---
+++ src/WorkTimer.EF/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace WorkTimer.EF
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int> {
+
+        public AppDbContext([NotNull] DbContextOptions options) : base(options) {
+
+        }
+
+        protected AppDbContext() {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
