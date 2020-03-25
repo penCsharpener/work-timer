414174130c4936305d829f145029458065a1d1b2
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-03-25 23:10:02 +0100

    fix ef configurationnn so that migration run at startup

--- src/WorkTimer.Api/Extensions/ServiceExtensions.cs
+++ src/WorkTimer.Api/Extensions/ServiceExtensions.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
 using WorkTimer.EF;
 using WorkTimer.EF.Models;
 
 namespace WorkTimer.Api.Extensions {
-    public static class ServiceCollectionExtensions {
+    public static class ServiceExtensions {
         public static void AddIdentity(this IServiceCollection services, PasswordOptions options) {
             services.AddIdentity<AppUser, AppRole>(setup => setup.Password = options)
                     .AddEntityFrameworkStores<AppDbContext>()
                     .AddDefaultTokenProviders();
+        }
+
+        public static async Task MigrateDatabase(this IServiceProvider sp) {
+            await sp.GetService<AppDbContext>().Database.MigrateAsync();
         }
     }
 }
--- src/WorkTimer.Api/Program.cs
+++ src/WorkTimer.Api/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using WorkTimer.Api.Extensions;
 using WorkTimer.Api.Models.Config;
 
 namespace WorkTimer.Api {
@@ -8,13 +12,25 @@ namespace WorkTimer.Api {
 
         public static IConfiguration LaunchSettings { get; private set; }
 
-        public static void Main(string[] args) {
+        public static async Task Main(string[] args) {
             LaunchSettings = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", false, true)
                 .AddUserSecrets<Program>(true, true)
                 .Build();
 
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) {
+                var sp = scope.ServiceProvider;
+                try {
+                    await sp.MigrateDatabase();
+                } catch (System.Exception ex) {
+                    var logger = sp.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) {
