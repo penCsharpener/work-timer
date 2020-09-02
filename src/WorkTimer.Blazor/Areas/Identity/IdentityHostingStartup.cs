d1dbcc051c7a9578ed7834bb51045f0d715f46a2
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-09-03 00:36:24 +0200

    add scaffolded Identity pages

---
+++ src/WorkTimer.Blazor/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(WorkTimer.Blazor.Areas.Identity.IdentityHostingStartup))]
+namespace WorkTimer.Blazor.Areas.Identity {
+    public class IdentityHostingStartup : IHostingStartup {
+        public void Configure(IWebHostBuilder builder) {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
