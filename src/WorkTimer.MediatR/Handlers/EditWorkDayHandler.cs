6fbc8f126f60de53efb5efdbd832f811c592e965
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-09-05 15:25:53 +0200

    add form to change Work Day Type

---
+++ src/WorkTimer.MediatR/Handlers/EditWorkDayHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WorkTimer.MediatR.Responses;
+using WorkTimer.Persistence.Data;
+
+namespace WorkTimer.MediatR.Handlers {
+    public class EditWorkDayHandler : IRequestHandler<GetWorkDayDetailsResponse, bool> {
+        private readonly AppDbContext _context;
+        private readonly ILogger<EditWorkDayHandler> _logger;
+
+        public EditWorkDayHandler(AppDbContext context, ILogger<EditWorkDayHandler> logger) {
+            _context = context;
+            _logger = logger;
+        }
+
+        public Task<bool> Handle(GetWorkDayDetailsResponse request, CancellationToken cancellationToken) {
+            try {
+
+                _context.WorkDays.Update(request.WorkDay);
+
+                _context.SaveChanges();
+
+                return Task.FromResult(true);
+
+            } catch (Exception ex) {
+                _logger.LogError(ex, $"Could not update work day with id {request.WorkDay.Id}");
+
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
--- src/WorkTimer.MediatR/Responses/GetWorkDayDetailsResponse.cs
+++ src/WorkTimer.MediatR/Responses/GetWorkDayDetailsResponse.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using MediatR;
+using System.Collections.Generic;
 using WorkTimer.Domain.Models;
 
 namespace WorkTimer.MediatR.Responses {
-    public class GetWorkDayDetailsResponse {
+    public class GetWorkDayDetailsResponse : IRequest<bool> {
         public WorkDay WorkDay { get; set; }
         public ICollection<WorkingPeriod> WorkingPeriods { get; set; }
         public bool IsOpenWorkday { get; set; }
