192dc4f9ff47e56d3e27d5cd09b085e312576d35
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-09-06 16:05:34 +0200

    fix MatDatePicker display correct time zone

--- src/WorkTimer.MediatR/Responses/GetWorkingPeriodResponse.cs
+++ src/WorkTimer.MediatR/Responses/GetWorkingPeriodResponse.cs
@@ -1,8 +1,31 @@
 ﻿using MediatR;
+using System;
 using WorkTimer.Domain.Models;
 
 namespace WorkTimer.MediatR.Responses {
     public class GetWorkingPeriodResponse : IRequest<bool> {
+
         public WorkingPeriod WorkingPeriod { get; set; }
+        public DateTime StartTime {
+            get => WorkingPeriod.StartTime.ToUniversalTime();
+            set => WorkingPeriod.StartTime = value;
+        }
+
+        public DateTime? EndTime {
+            get {
+                if (WorkingPeriod.EndTime.HasValue) {
+                    return WorkingPeriod.EndTime.Value.ToUniversalTime();
+                }
+                return WorkingPeriod.EndTime;
+            }
+
+            set {
+                if (value.HasValue) {
+                    WorkingPeriod.EndTime = value.Value;
+                } else {
+                    WorkingPeriod.EndTime = value;
+                }
+            }
+        }
     }
 }
