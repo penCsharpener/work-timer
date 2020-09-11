aedd191e9ee5de17398895e2e811bb1f1b335423
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-09-11 21:03:58 +0200

    update nugets, add serilog

--- src/WorkTimer.Blazor/Program.cs
+++ src/WorkTimer.Blazor/Program.cs
@@ -1,41 +1,50 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
+using Serilog;
 using WorkTimer.Dev.Seeding;
 using WorkTimer.Persistence.Extensions;
 
 namespace WorkTimer.Blazor {
     public sealed class Program {
-        public static IConfiguration LaunchSettings { get; private set; }
+        public static IConfiguration? LaunchSettings { get; private set; }
 
         public static async Task Main(string[] args) {
+            LaunchSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddUserSecrets<Program>(true, true)
+                .Build();
+
             try {
-                LaunchSettings = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json", false, true)
-                    .AddUserSecrets<Program>(true, true)
-                    .Build();
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(LaunchSettings)
+                    .CreateLogger();
 
-                var host = CreateHostBuilder(args).Build();
+                Log.Information("Starting up");
 
-                using (var scope = host.Services.CreateScope()) {
-                    var sp = scope.ServiceProvider;
+                IHost? host = CreateHostBuilder(args).Build();
+
+                using (IServiceScope? scope = host.Services.CreateScope()) {
+                    IServiceProvider? sp = scope.ServiceProvider;
+
                     try {
                         await sp.MigrateDatabaseAsync();
                         await sp.SeedDatabaseAsync();
                     } catch (Exception ex) {
-                        var logger = sp.GetRequiredService<ILogger<Program>>();
+                        ILogger<Program>? logger = sp.GetRequiredService<ILogger<Program>>();
                         logger.LogError(ex, "An error occurred while migrating the database.");
                     }
                 }
 
-                host.Run();
-
-            } catch {
-                // no serilog yet
+                await host.RunAsync();
+            } catch (Exception e) {
+                Log.Fatal(e, "Application start-up failed");
+            } finally {
+                Log.CloseAndFlush();
             }
         }
 
@@ -43,8 +52,8 @@ namespace WorkTimer.Blazor {
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>()
-                              .UseUrls("http://localhost:4660;https://localhost:4661");
-                });
+                        .UseUrls("http://localhost:4660;https://localhost:4661");
+                }).UseSerilog(Log.Logger);
         }
     }
 }
