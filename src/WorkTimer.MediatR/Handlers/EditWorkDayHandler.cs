8bd8c70d462f6872689ea645c0b3ce267287aaee
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-09-16 21:54:03 +0200

    change work day date automatically if all working periods have the same different date

--- src/WorkTimer.MediatR/Handlers/EditWorkDayHandler.cs
+++ src/WorkTimer.MediatR/Handlers/EditWorkDayHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using WorkTimer.MediatR.Responses;
@@ -18,6 +19,17 @@ namespace WorkTimer.MediatR.Handlers {
 
         public Task<bool> Handle(GetWorkDayDetailsResponse request, CancellationToken cancellationToken) {
             try {
+                var workingPeriods = _context.WorkingPeriods.Where(a => a.WorkDayId == request.WorkDay.Id).Select(x => new { x.StartTime.Date }).ToList();
+                if (workingPeriods?.Count > 0) {
+                    var count = workingPeriods.GroupBy(x => x.Date).Count();
+
+                    if (count == 1) {
+                        var date = workingPeriods.FirstOrDefault().Date;
+                        if (date != request.WorkDay.Date) {
+                            request.WorkDay.Date = date;
+                        }
+                    }
+                }
 
                 _context.WorkDays.Update(request.WorkDay);
 
