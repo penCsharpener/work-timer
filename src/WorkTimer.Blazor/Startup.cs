14136e62c9600e0f8fc117cd136e295bdc03c13a
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-12-02 23:53:54 +0100

    set german global culture

--- src/WorkTimer.Blazor/Startup.cs
+++ src/WorkTimer.Blazor/Startup.cs
@@ -19,8 +19,6 @@ namespace WorkTimer.Blazor {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddEntityFramework(Configuration);
             services.AddHttpContextAccessor();
@@ -32,7 +30,6 @@ namespace WorkTimer.Blazor {
             services.AddScoped<TokenProvider>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
